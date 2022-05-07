67b2db74322dcaeffc33eec67835323e821bb351
mrsimplelife <mrsimplelife@naver.com>
2022-05-08 04:18:08 +0900

    2 files modified

--- src/longestPalindrome/longestPalindrome.test.ts
+++ src/longestPalindrome/longestPalindrome.test.ts
@@ -1,4 +1,6 @@
-import longestPalindrome from 'longestPalindrome/longestPalindrome';
+import longestPalindrome, {
+  isPalindromic,
+} from 'longestPalindrome/longestPalindrome';
 
 test(`
 Input: s = "babad"
@@ -6,14 +8,42 @@ Output: "bab"
 Explanation: "aba" is also a valid answer.
 `, () => {
   const result = longestPalindrome('babad');
-  const expected = 'bab';
-  expect(result).toEqual(expected);
+
+  expect(isPalindromic(result)).toBeTruthy();
+  expect(result.length).toBe(3);
 });
 test(`
 Input: s = "cbbd"
 Output: "bb"
 `, () => {
   const result = longestPalindrome('cbbd');
-  const expected = 'bb';
-  expect(result).toEqual(expected);
+  expect(isPalindromic(result)).toBeTruthy();
+  expect(result.length).toBe(2);
+});
+test(`
+Input:"ccd"
+Output:"cc"
+`, () => {
+  const result = longestPalindrome('ccd');
+  expect(isPalindromic(result)).toBeTruthy();
+  expect(result.length).toBe(2);
+});
+test(`
+Input:"eabcb"
+Output:"bcb"
+`, () => {
+  const result = longestPalindrome('eabcb');
+  expect(isPalindromic(result)).toBeTruthy();
+  expect(result.length).toBe(3);
+});
+test.only(`
+Input:"vaomxdtiuwqlwhgutkhxxhccsgvyoaccuicgybnqnslogtqhblegfudagpxfvjdacsxgevvepuwthdtybgflsxjdmmfumyqgpxatvdypjmlapccaxwkuxkilqqgpihyepkilhlfkdrbsefinitdcaghqmhylnixidrygdnzmgubeybczjceiybowglkywrpkfcwpsjbkcpnvfbxnpuqzhotzspgebptnhwevbkcueyzecdrjpbpxemagnwmtwikmkpqluwmvyswvxghajknjxfazshsvjkstkezdlbnkwxawlwkqnxghjzyigkvqpapvsntojnxlmtywdrommoltpbvxwqyijpkirvndwpgufgjelqvwffpuycqfwenhzrbzbdtupyutgccdjyvhptnuhxdwbmdcbpfvxvtfryszhaakwshrjseonfvjrrdefyxefqfvadlwmedpvnozobftnnsutegrtxhwitrwdpfienhdbvvykoynrsbpmzjtotjxbvemgoxreiveakmmbbvbmfbbnyfxwrueswdlxvuelbkrdxlutyukppkzjnmfmclqpkwzyylwlzsvriwomchzzqwqglpflaepoxcnnewzstvegyaowwhgvcwjhbbstvzhhvghigoazbjiikglbqlxlccrwqvyqxpbtpoqjliziwmdkzfsrqtqdkeniulsavsfqsjwnvpprvczcujihoqeanobhlsvbzmgflhykndfydbxatskf"
+Output:"bcb"
+`, () => {
+  const result = longestPalindrome(
+    'vaomxdtiuwqlwhgutkhxxhccsgvyoaccuicgybnqnslogtqhblegfudagpxfvjdacsxgevvepuwthdtybgflsxjdmmfumyqgpxatvdypjmlapccaxwkuxkilqqgpihyepkilhlfkdrbsefinitdcaghqmhylnixidrygdnzmgubeybczjceiybowglkywrpkfcwpsjbkcpnvfbxnpuqzhotzspgebptnhwevbkcueyzecdrjpbpxemagnwmtwikmkpqluwmvyswvxghajknjxfazshsvjkstkezdlbnkwxawlwkqnxghjzyigkvqpapvsntojnxlmtywdrommoltpbvxwqyijpkirvndwpgufgjelqvwffpuycqfwenhzrbzbdtupyutgccdjyvhptnuhxdwbmdcbpfvxvtfryszhaakwshrjseonfvjrrdefyxefqfvadlwmedpvnozobftnnsutegrtxhwitrwdpfienhdbvvykoynrsbpmzjtotjxbvemgoxreiveakmmbbvbmfbbnyfxwrueswdlxvuelbkrdxlutyukppkzjnmfmclqpkwzyylwlzsvriwomchzzqwqglpflaepoxcnnewzstvegyaowwhgvcwjhbbstvzhhvghigoazbjiikglbqlxlccrwqvyqxpbtpoqjliziwmdkzfsrqtqdkeniulsavsfqsjwnvpprvczcujihoqeanobhlsvbzmgflhykndfydbxatskf'
+  );
+  console.log(result);
+  // expect(isPalindromic(result)).toBeTruthy();
+  // expect(result.length).toBe(3);
 });
--- src/longestPalindrome/longestPalindrome.ts
+++ src/longestPalindrome/longestPalindrome.ts
@@ -1,5 +1,21 @@
 function longestPalindrome(s: string): string {
-  return '';
+  let res = '';
+  for (let i = 0; i < s.length; i++) {
+    let curr = '';
+    for (let j = i; j < s.length; j++) {
+      curr += s[j];
+      if (s[i] === s[j] && res.length < curr.length && isPalindromic(curr)) {
+        res = curr;
+      }
+    }
+  }
+  return res;
 }
 
+export const isPalindromic = (s: string) => {
+  return s === s.split('').reverse().join('');
+};
+
 export default longestPalindrome;
+
+// c b b d
