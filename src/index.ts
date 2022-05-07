6ebbc577e4fe814950659265fe40f200aa2658ee
mrsimplelife <mrsimplelife@naver.com>
2022-05-08 05:41:39 +0900

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import longestPalindrome from 'longestPalindrome/longestPalindrome';
+
 console.log(`node version : ${process.version}`);
+const result = longestPalindrome(
+  'selfdltmpujlnhfzjcgnbgprvopxklmlgrlbldzpnkhvhkybpgtzipzotrgzkdrqntnuaqyaplcybqyvidw'
+);
+console.log(result);
