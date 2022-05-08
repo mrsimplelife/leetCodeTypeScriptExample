faae08de7e3f64ca934905ef6e63182be110583e
mrsimplelife <mrsimplelife@naver.com>
2022-05-08 13:17:05 +0900

    1 file modified

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import longestPalindrome from 'longestPalindrome/longestPalindrome';
+
 console.log(`node version : ${process.version}`);
+const result = longestPalindrome('cbbd');
+console.log(result);
