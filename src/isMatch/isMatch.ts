0541093a6fee6e611da6069132a95189ee17f8ad
mrsimplelife <mrsimplelife@naver.com>
2022-06-26 13:49:23 +0900

    1 file modified

--- src/isMatch/isMatch.ts
+++ src/isMatch/isMatch.ts
@@ -1,5 +1,23 @@
+// function isMatch(s: string, p: string): boolean {
+//   return false;
+// }
+
 function isMatch(s: string, p: string): boolean {
-  return false;
+  const dp = new Array(s.length + 1)
+    .fill(null)
+    .map(() => new Array(p.length + 1).fill(false));
+  dp[s.length][p.length] = true;
+  for (let i = s.length; i >= 0; i--) {
+    for (let j = p.length - 1; j >= 0; j--) {
+      const firstMatch = i < s.length && new Set([s[i], '.']).has(p[j]);
+      if (j + 1 < p.length && p[j + 1] === '*') {
+        dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+      } else {
+        dp[i][j] = firstMatch && dp[i + 1][j + 1];
+      }
+    }
+  }
+  return dp[0][0];
 }
 
 export default isMatch;
