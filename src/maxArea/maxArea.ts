f3dc00d9c35abd05941bf61e7d07a15a4ba75585
mrsimplelife <mrsimplelife@naver.com>
2022-06-28 05:56:24 +0900

    1 file modified

--- src/maxArea/maxArea.ts
+++ src/maxArea/maxArea.ts
@@ -22,18 +22,4 @@ function maxArea(height: number[]): number {
   }
   return res;
 }
-// def maxArea(self, height: List[int]) -> int:
-// maxarea = 0
-// left = 0
-// right = len(height) - 1
-
-// while left < right:
-//     width = right - left
-//     maxarea = max(maxarea, min(height[left], height[right]) * width)
-//     if height[left] <= height[right]:
-//         left += 1
-//     else:
-//         right -= 1
-
-// return maxarea
 export default maxArea;
